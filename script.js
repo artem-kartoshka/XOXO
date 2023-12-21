503e4be52e54514ea78fb956c9cca20fe13b3315
artem-kartoshka <a.kukaev.16@gmail.com>
2023-12-21 19:32:39 +0300

    rabotaet

--- script.js
+++ script.js
@@ -1,22 +1,53 @@
 function randint(min,max){
     return Math.round(Math.random()*(max-min)+ min)}
+function check(elements){
+    if (
+           (elements[0]==1 && elements[1]==1 && elements[2]==1)
+        || (elements[3]==1 && elements[4]==1 && elements[5]==1)
+        || (elements[6]==1 && elements[7]==1 && elements[8]==2)
+        || (elements[0]==1 && elements[3]==1 && elements[6]==2)
+        || (elements[1]==1 && elements[4]==1 && elements[7]==2)
+        || (elements[2]==1 && elements[5]==1 && elements[8]==2)
+        || (elements[0]==1 && elements[4]==1 && elements[8]==2)
+        || (elements[2]==1 && elements[4]==1 && elements[6]==2)  
+     ) {
+        console.log("red")
+        pole.style.display="none"
 
+    }
+    if (
+           (elements[0]==2 && elements[1]==2 && elements[2]==2) 
+        || (elements[3]==2 && elements[4]==2 && elements[5]==2)
+        || (elements[6]==2 && elements[7]==2 && elements[8]==2)
+        || (elements[0]==2 && elements[3]==2 && elements[6]==2)
+        || (elements[1]==2 && elements[4]==2 && elements[7]==2)
+        || (elements[2]==2 && elements[5]==2 && elements[8]==2)
+        || (elements[0]==2 && elements[4]==2 && elements[8]==2)
+        || (elements[2]==2 && elements[4]==2 && elements[6]==2) ) {
+        console.log("blue")
+        pole.style.display="none"
+    }
+}
 let nameJS=document.querySelector('.name')
 let startJS=document.querySelector('.start')
 let questionJS = document.querySelector('.question')
 let red = document.querySelector('.choice1')
 let blue = document.querySelector('.choice2')
 let random = document.querySelector('.choice3')
-let resultJS = document.querySelector('.result')
+let resultr = document.querySelector('.result_r')
+let resultb = document.querySelector('.result_b')
 let pole = document.querySelector('.box')
 let fields=document.querySelectorAll('.yach')
 let first=0
+let elements=[0,0,0,0,0,0,0,0,0]
+let fieldsJS=[]
 
 nameJS.style.display="flex"
 startJS.style.display="flex"
 questionJS.style.display="none"
 pole.style.display="none"
-resultJS.style.display="none"
+resultr.style.display="none"
+resultb.style.display="none"
 red.style.display="none"
 blue.style.display="none"
 random.style.display="none"
@@ -59,27 +90,26 @@ random.addEventListener('click',function(){
     questionJS.style.display="none"
 })
 
-<<<<<<< HEAD
 
 
 for (let i = 0; i < 9; i++){
     fields[i].addEventListener('click', function() {
     if (first % 2 == 1){
         fields[i].innerHTML=`<img src="red.png"/>`
+        fieldsJS=document.querySelectorAll('.yach')
+        //console.log(fieldsJS)
+        elements[i]=1
+        console.log(elements)
         first++
+        check(elements)
     }
         else{
             fields[i].innerHTML=`<img src="blue.png"/>`
+            fieldsJS=document.querySelectorAll('.yach')
+            //console.log(fieldsJS)
             first++
-    }})}
-=======
-})
-if (first == 1){image=red}else{image=blue} 
-
-
-
-fields[1].addEventListener('click',function(){
-    fields[1].style.background(image)
-})
-
->>>>>>> 967e129852e5dd0baeac7675188910fae4d30caf
+            elements[i]=2
+            console.log(elements)
+            check(elements)
+    }
+    })}
